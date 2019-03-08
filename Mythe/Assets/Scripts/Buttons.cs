c28047ff9a71567647b1bfb465efa505656f1545
BrandonC <brandoncoenraads@gmail.com>
2019-03-08 11:02:00 +0100

    startmenu
    startmenu

---
+++ Mythe/Assets/Scripts/Buttons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Buttons : MonoBehaviour
+{
+
+   
+
+    void Start()
+    {
+    
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnClick()
+    {
+     
+    }
+
+    public void startscene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Optionscene()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+}
