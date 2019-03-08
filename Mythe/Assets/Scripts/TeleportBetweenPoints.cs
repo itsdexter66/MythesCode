335a8d2f0723ea1c4ce287755a09cd653d565e0d
MSI\user <semmyking@gmail.com>
2019-03-08 13:55:52 +0100

    TeleportScript
    + added a teleport script

---
+++ Mythe/Assets/Scripts/TeleportBetweenPoints.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportBetweenPoints : MonoBehaviour
+{
+    public Vector3 TeleportPoint;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.transform.position = TeleportPoint;
+        }
+    }
+}
