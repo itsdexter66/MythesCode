8627b8f0b4f16b2fc577daafefa6f2949ef9e696
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2019-03-11 10:52:57 +0100

    Added the core of the game

---
+++ Mythe/Assets/Scripts/Jelle/InterfaceFunctions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceFunctions : MonoBehaviour
+{
+    string gameScene = "JelleScene1";
+    public void Restart()
+    {
+        Application.LoadLevel(gameScene);
+    }
+}
