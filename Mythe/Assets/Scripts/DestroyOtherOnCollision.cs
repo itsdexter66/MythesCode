dd9ac5bc4e095be4155366ad134aa821abc9e672
MSI\user <semmyking@gmail.com>
2019-03-11 14:15:55 +0100

    Cave beta test scene
    made a scene for the cave.
    + death barrier and spikes (bothe using Destroyoncoll. script)
    + test parkout for physics
    + test entrance cave to have a indication for 2d/3d environment
    + added test material

---
+++ Mythe/Assets/Scripts/DestroyOtherOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOtherOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        // Destoy other object on collision with the tag Player
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
--- Mythe/Assets/Scripts/TeleportBetweenPoints.cs
+++ Mythe/Assets/Scripts/TeleportBetweenPoints.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeleportBetweenPoints : MonoBehaviour
-{
+public class TeleportBetweenPoints : MonoBehaviour { 
+    // x,y,z position where you will be teleported to on collision
     public Vector3 TeleportPoint;
 
-    private void OnCollisionEnter(Collision col)
+    private void OnCollisionEnter(Collision other)
     {
-        if (col.gameObject.tag == "Player")
+        // you need a gameObject with the tag Player 
+        if (other.gameObject.tag == "Player")
         {
-            col.transform.position = TeleportPoint;
+            // transform the position of the object that touched this object
+            other.transform.position = TeleportPoint;
         }
     }
 }
