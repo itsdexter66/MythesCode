a1f6be552f3631b6fb72348c98460d65cc37d58a
BrandonC <brandoncoenraads@gmail.com>
2019-03-22 09:01:52 +0100

    sound
    sound oncollision

---
+++ Mythe/Assets/Scripts/BtnPressed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnPressed : MonoBehaviour
+{
+
+    public Action Escpressed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Hitesc();
+    }
+
+    void Hitesc()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("cool");
+            //Escpressed();
+        }
+    }
+}
