acddbff1b68b3d37a3a3a6c2c3d24154aa5d00e1
MSI\user <semmyking@gmail.com>
2019-03-29 11:04:19 +0100

    level1 chasingpart done
    made level design for the chasing part

--- Mythe/Assets/Scripts/Jelle/CameraScript.cs
+++ Mythe/Assets/Scripts/Jelle/CameraScript.cs
@@ -22,8 +22,8 @@ public class CameraScript : MonoBehaviour
 
     private void LockOnTarget()
     {
-        float dist = Vector3.Distance(transform.position, targetT.position);
-        transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetT.position.x, 0.1f * moveToPlayerSpeed), Mathf.Lerp(transform.position.y, targetT.position.y, 0.1f * moveToPlayerSpeed) + 0.2f, -17);
-        //transform.position = new Vector3(targetT.position.x, targetT.position.y + 2, -17);
+       // float dist = Vector3.Distance(transform.position, targetT.position);
+        //transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetT.position.x, 0.1f * moveToPlayerSpeed), Mathf.Lerp(transform.position.y, targetT.position.y, 0.1f * moveToPlayerSpeed) + 0.2f, -17);
+        transform.position = new Vector3(targetT.position.x, targetT.position.y + 4, -17);
     }
 }
