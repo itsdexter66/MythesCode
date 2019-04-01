c198cd276524805cb496f7888382bbc5d3165b47
DESKTOP-GDM5KI7\Mreow <Sevarish@hotmail.com>
2019-04-01 14:55:47 +0200

    no message

---
+++ Mythe/Assets/Scripts/Jelle/MobileInputHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileInputHandler : MonoBehaviour
+{
+    [SerializeField]
+    string currentButton;
+    Image spriteButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteButton = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mew");
+        if (currentButton == "Right")
+        {
+            Debug.Log("RightB");
+        }
+
+        if (currentButton == "Left")
+        {
+            Debug.Log("LeftB");
+        }
+    }
+
+}
