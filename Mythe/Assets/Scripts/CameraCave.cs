579a53cc26bca32f32dec5903aebd307b9096fe3
MSI\user <semmyking@gmail.com>
2019-04-15 13:52:48 +0200

    Issues fixed

--- Mythe/Assets/Scripts/CameraCave.cs
+++ Mythe/Assets/Scripts/CameraCave.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraCaveScript : MonoBehaviour
+public class CameraCave : MonoBehaviour
 {
     Transform targetPlayer;
     readonly string targetName = "Player";
--- Mythe/Assets/Scripts/InvisibleColliders/OnCollSetActive.cs
+++ Mythe/Assets/Scripts/InvisibleColliders/OnCollSetActive.cs
@@ -2,29 +2,19 @@
 
 public class OnCollSetActive : MonoBehaviour
 {
+    public string tag;
     // objects that need to be set active or false
     [SerializeField]
     private GameObject objectsSetActive,
                        objectsSetFalse;
 
-    // booleans to tell if the objects needs to be set active or false
-    [SerializeField]
-    private bool activityObjectTrue,
-                 activityObjectFalse;
-
     private void OnCollisionEnter(Collision other)
     {
         // if on collision with player, let object appear
-        if (other.gameObject.tag == "Player")
-        {
-            if (activityObjectTrue == true )
-            {
-                objectsSetActive.SetActive(true);
-            }
-            if (activityObjectFalse == true)
-            {
-                objectsSetFalse.SetActive(false);
-            }
+        if (other.gameObject.tag == tag)
+        {   
+            objectsSetActive.SetActive(true);
+            objectsSetFalse.SetActive(false);
             Destroy(this.gameObject);
         }
     }
